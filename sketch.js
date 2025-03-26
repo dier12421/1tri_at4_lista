dde51e6e78507ebfe4cf1e8c6b3d8f96b2358e5f
dier <161588493+dier12421@users.noreply.github.com>
2025-03-26 08:29:55 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,6 @@
+let paises = ["Brasil","Estados Unidos", "Índia", "França", "Venezuela", "Ítalia", "Russia", "Paraguai", "Argentina", "Alemanha"]
+
+for (indice=0; indice<=9; indice++) {
+ console.log(paises[indice]);
+
+}
